d714fc6c05657a40fcd0fbd9c9b2bf9b190d3048
moon <qt1998@naver.com>
2021-09-29 11:53:28 +0900

    start project

---
+++ Facility_Backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from database import engine
+import models
+from routers import main, building_mgmt, facility_mgmt, login
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(login.router)
+app.include_router(main.router)
+app.include_router(building_mgmt.router)
+app.include_router(facility_mgmt.router)
